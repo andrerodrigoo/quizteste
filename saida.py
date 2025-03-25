1686a0bcd60d2b4767fd2ddfd308460beddda339
andrerodrigoo <andremaropo2015@gmail.com>
2025-03-25 20:39:40 -0300

    Add files via upload

---
+++ saida.py
@@ -0,0 +1,7 @@
+print("Ola, sejá bem vindo(a)")
+
+print("O valor inteiro em decimal é: (10: d) ")
+print("O valor inteiro em binário é: (10: b) ")
+
+print(f"O valor de pi é: {3.14159265: f}")
+print(f"O valor de pi é: {3.14159265: .2f}")
